e93f5c777e6589d16b7a066c628f8a1441143cd3
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-08-19 16:36:13 +0900

    Recaps

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  painGreetings(username);
+  paintGreetings(username);
 }
 
-function painGreetings(username) {
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -24,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  painGreetings(savedUsername);
+  paintGreetings(savedUsername);
 }
